cd74b369ed6ada96c9c4ad0f57dc67d7bc95d772
varunriyer <vs7835@srmist.edu.in>
2025-05-25 15:19:06 +0530

    Add initial quiz questions and setup in script.js

---
+++ script.js
@@ -0,0 +1,40 @@
+const { text } = require("express");
+
+const questions = [
+    {
+        question: "Which country has 3 capital cities?",
+        answers: [
+            { text: "Mexico", correct: false },
+            { text: "South Africa", correct: true },
+            { text: "Brazil", correct: false },
+            { text: "New Zealand", correct: false }
+        ]
+    },
+    {
+        question: "Which animal has 3 stomachs?",
+        answers: [
+            { text: "Camel", correct: false },
+            { text: "Giraffe", correct: false },
+            { text: "Platypus", correct: false },
+            { text: "Cow", correct: true }
+        ]
+    },
+    {
+        question: "Name the band which made Mr. Brightside?",
+        answers: [
+            { text: "The Killers", correct: true },
+            { text: "Goo Goo Dolls", correct: false },
+            { text: "Snow Patrol", correct: false },
+            { text: "Fleetwood Mac", correct: false }
+        ]
+    }
+];
+
+const questionElement = document.getElementById("question");
+const answerButton = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("nxt-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+
