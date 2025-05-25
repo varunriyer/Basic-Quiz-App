bac8e20726b0cce9b8fb8b4c42c46b3cb8850c79
varunriyer <vs7835@srmist.edu.in>
2025-05-25 15:42:04 +0530

    Add correct answer data attribute and improve answer selection logic

--- script.js
+++ script.js
@@ -53,7 +53,10 @@ function showQuestion() {
         button.innerHTML = answer.text;
         button.classList.add("btn");
         answerButtons.appendChild(button);
-
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer)
     });
 }
 
@@ -61,6 +64,17 @@ function resetState() {
     nextButton.style.display = "none";
     while (answerButtons.firstChild) {
         answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+function selectAnswer(e) {
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if (isCorrect) {
+        selectedBtn.classList.add("correct");
+    }
+    else {
+        selectedBtn.classList.add("incorrect");
     }
 }
 
